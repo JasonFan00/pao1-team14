1648b997f2317c82ea97c331d69ee93a1b6aee6a
Jason <47374783+JasonFang00@users.noreply.github.com>
2022-02-18 01:34:39 -0500

    m

--- pa01/schedule.py
+++ pa01/schedule.py
@@ -18,6 +18,7 @@ class Schedule():
     def load_courses(self):
         ''' load_courses reads the course data from the courses.json file'''
         print('getting archived regdata from file')
+        # with open("pa01/courses20-21.json", "r", encoding='utf-8') as jsonfile: debugging due to import paths
         with open("courses20-21.json", "r", encoding='utf-8') as jsonfile:
             courses = json.load(jsonfile)
         for course in courses:
