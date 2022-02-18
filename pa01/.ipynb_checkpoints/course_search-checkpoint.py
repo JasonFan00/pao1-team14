b87efd62c9cadb7a785b74c797c609a3b4718904
Sampada Pokharel <2000+pokharelsampada@users.noreply.github.com>
2022-02-17 22:48:33 -0500

    5a and 5b

--- pa01/.ipynb_checkpoints/course_search-checkpoint.py
+++ pa01/.ipynb_checkpoints/course_search-checkpoint.py
@@ -64,5 +64,8 @@ def print_course(course):
     print(course['subject'],course['coursenum'],course['section'],
           course['name'],course['term'],course['instructor'])
 
+
+
+
 if __name__ == '__main__':
     topmenu()
